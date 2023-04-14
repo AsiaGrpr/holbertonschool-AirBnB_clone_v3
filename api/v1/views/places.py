423fe3a8861dc8b02ccf0cb0c1b44cc478582cbf
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-14 09:01:08 +0000

    Create function to add a new place in a city

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -77,9 +77,44 @@ def update_place(place_id):
     if not old:
         return abort(404)
 
-    ignore = ['id', 'created_at', 'updated_at']
+    ignore = ['id', 'created_at', 'updated_at', 'user_id', 'city_id']
     for key, value in new.items():
         if key not in ignore:
             setattr(old, key, value)
     storage.save()
     return make_response(jsonify(old.to_dict()), 200)
+
+
+@app_views.route('/cities/<city_id>/places', methods=['POST'],
+                 strict_slashes=False)
+def cities_places(city_id):
+    """get places of a city"""
+
+    city = storage.get(City, city_id)
+    if not city:
+        return abort(404)
+    
+    new = request.get_json(silent=True)
+    if not new:
+        return abort(400, {"Not a JSON"})
+    
+    if "user_id" not in new:
+        return abort(400, {"Missing user_id"})
+    
+    user = storage.get(User, city.user_id)
+    
+    if not user:
+        return abort(404)
+    
+    if "name" not in new:
+        return abort(400, {"Missing name"})
+    
+    new_obj = Place(name=new_state['name'], city_id=city.id)
+    new_obj.update(new)
+    storage.new(new_obj)
+    storage.save()
+    return make_response(new_obj.to_dict(), 201)
+
+    
+     
+   
